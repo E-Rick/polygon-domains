0e67bae5650ded02d3858a49054db3e34c026881
Erick Martinez <44077419+E-Rick@users.noreply.github.com>
2022-05-11 21:13:49 -0400

    add: stringutils helper and tld domain for payable

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -2,26 +2,49 @@
 
 pragma solidity ^0.8.10;
 
+// Don't forget to add this import
+import {StringUtils} from "./libraries/StringUtils.sol";
 import "hardhat/console.sol";
 
 contract Domains {
+    // Here's our domain TLD!
+    string public tld;
+
     // A "mapping" data type to store their names
     mapping(string => address) public domains;
-
-    // Checkout our new mapping! This will store values
     mapping(string => string) public records;
 
-    // Stores the nft addresses associated with domain
-    mapping(string => string) public nfts;
+    // We make the contract "payable" by adding this to the constructor
+    constructor(string memory _tld) payable {
+        tld = _tld;
+        console.log("%s name service deployed", _tld);
+    }
 
-    constructor() {
-        console.log("WRECS DOMAIN SERVICE CONTRACT.");
+    // This function will give us the price of a domain based on length
+    function price(string calldata name) public pure returns (uint256) {
+        uint256 len = StringUtils.strlen(name);
+        require(len > 0);
+        if (len == 3) {
+            return 5 * 10**17; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). We're going with 0.5 Matic cause the faucets don't give a lot
+        } else if (len == 4) {
+            return 3 * 10**17; // To charge smaller amounts, reduce the decimals. This is 0.3
+        } else {
+            return 1 * 10**17;
+        }
     }
 
     // A register function that adds their names to our mapping
     function register(string calldata name) public {
         // Check that the name is unregistered (explained in notes)
         require(domains[name] == address(0));
+        domains[name] = msg.sender;
+        console.log("%s has registered a domain!", msg.sender);
+
+        uint256 _price = price(name);
+
+        // Check if enough Matic was paid in the transaction
+        require(msg.value >= _price, "Not enough Matic paid");
+
         domains[name] = msg.sender;
         console.log("%s has registered a domain!", msg.sender);
     }
